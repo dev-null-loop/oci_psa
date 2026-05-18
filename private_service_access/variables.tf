00246c8243b145ae3148cac80fb1544ae83f283d
dev-null-loop <dev.null.loop@gmail.com>
2026-05-18 15:39:38 +0300

    Fix PSA private service access variable descriptions

--- private_service_access/variables.tf
+++ private_service_access/variables.tf
@@ -1,56 +1,56 @@
 variable "compartment_id" {
-  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment to contain the private service access."
+  description = "(Required) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment to contain the private service access."
   type        = string
 }
 
 variable "defined_tags" {
-  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
   type        = map(string)
   default     = null
 }
 
 variable "description" {
-  description = "A description of this private service accesss."
+  description = "(Optional) (Updatable) A description of this private service accesss."
   type        = string
   default     = null
 }
 
 variable "display_name" {
-  description = "A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
+  description = "(Optional) (Updatable) A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
   type        = string
   default     = null
 }
 
 variable "freeform_tags" {
-  description = "Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
   type        = map(string)
   default     = {}
 }
 
 variable "ipv4ip" {
-  description = "The private IPv4 address (in the consumer's VCN) that represents the access point for the associated service."
+  description = "(Optional) The private IPv4 address (in the consumer's VCN) that represents the access point for the associated service."
   type        = string
   default     = null
 }
 
 variable "nsg_ids" {
-  description = "A list of the OCIDs of the network security groups (NSGs) to add the private  service access's VNIC to.  For more information about NSGs, see [NetworkSecurityGroup](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/latest/NetworkSecurityGroup/)."
+  description = "(Optional) (Updatable) A list of the OCIDs of the network security groups (NSGs) to add the private  service access's VNIC to.  For more information about NSGs, see [NetworkSecurityGroup](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/latest/NetworkSecurityGroup/)."
   type        = list(string)
   default     = []
 }
 
 variable "security_attributes" {
-  description = "[Security attributes](https://docs.cloud.oracle.com/iaas/Content/zero-trust-packet-routing/zpr-artifacts.htm#security-attributes) are labels for a resource that can be referenced in a [Zero Trust Packet Routing](https://docs.cloud.oracle.com/iaas/Content/zero-trust-packet-routing/overview.htm) (ZPR) policy to control access to ZPR-supported resources.  Example: `{\"Oracle-DataSecurity-ZPR\": {\"MaxEgressCount\": {\"value\":\"42\",\"mode\":\"audit\"}}}`"
+  description = "(Optional) (Updatable) [Security attributes](https://docs.cloud.oracle.com/iaas/Content/zero-trust-packet-routing/zpr-artifacts.htm#security-attributes) are labels for a resource that can be referenced in a [Zero Trust Packet Routing](https://docs.cloud.oracle.com/iaas/Content/zero-trust-packet-routing/overview.htm) (ZPR) policy to control access to ZPR-supported resources.  Example: `{\"Oracle-DataSecurity-ZPR\": {\"MaxEgressCount\": {\"value\":\"42\",\"mode\":\"audit\"}}}`"
   type        = map(string)
   default     = null
 }
 
 variable "service_id" {
-  description = "A unique service identifier for which the private service access was created."
+  description = "(Required) A unique service identifier for which the private service access was created."
   type        = string
 }
 
 variable "subnet_id" {
-  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VCN's subnet where the private service access's VNIC will reside."
+  description = "(Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VCN's subnet where the private service access's VNIC will reside."
   type        = string
 }
